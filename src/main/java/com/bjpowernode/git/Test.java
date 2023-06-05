4b5bd15a9874e5e69297ac5d73ab6e67545d2ff5
xiaofei <773211139@qq.com>
2023-06-05 16:49:45 +0800

    first commit

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -1,18 +1,12 @@
 package com.bjpowernode.git;
 
-/**
- * @Program:Test
- * @Description: TODO
- * @Author: Mr.deng
- * @DATE: 2023/6/5
- */
+
 public class Test {
     public static void main(String[] args) {
 
         System.out.println("你好,Git");
         System.out.println("hello,Git");
         System.out.println("抠你吉瓦,Git");
-        System.out.println("hot-fix test");
         System.out.println("2222222222222");
     }
 }
