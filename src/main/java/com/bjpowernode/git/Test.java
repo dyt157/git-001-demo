a5914502274c4ebd09cdb3bb26b0667403fe26c1
xiaofei <773211139@qq.com>
2023-06-05 18:12:12 +0800

    commit

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -4,7 +4,7 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("你好,Git222");
+        System.out.println("你好,Git222333");
         System.out.println("hello,Git");
         System.out.println("抠你吉瓦,Git");
         System.out.println("2222222222222333");
