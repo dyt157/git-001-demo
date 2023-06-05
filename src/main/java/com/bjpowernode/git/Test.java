80fe03ff534d34255fe49caf300b6f2a20ad2ebc
xiaofei <773211139@qq.com>
2023-06-05 18:10:08 +0800

    delete ai and rika

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -7,7 +7,7 @@ public class Test {
         System.out.println("你好,Git");
         System.out.println("hello,Git");
         System.out.println("抠你吉瓦,Git");
-        System.out.println("2222222222222");
+        System.out.println("2222222222222333");
         System.out.println("master test");
     }
 }
